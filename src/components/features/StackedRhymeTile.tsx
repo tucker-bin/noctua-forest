a1d92e0adc82dd4a47d2a4f4547898b84af1d8f1
tucker.apply@gmail.com <tucker.apply@gmail.com>
2025-08-07 22:38:25 -0400

    publish: site update

--- src/components/features/StackedRhymeTile.tsx
+++
@@ -1,6 +0,0 @@
-// This file is now deprecated - functionality moved to RhymeMahjong.tsx
-// The TraditionalMahjongTile component is now defined inline for better integration
-
-export { default as RhymeMahjong } from './RhymeMahjong';
-
- 
--- src/pages/Credit.tsx
+++
@@ -1,6 +0,0 @@
-const PLAN_OPTIONS = [
-  { name: "Starter", price: 0.54, tokens: 100, stripePriceId: "price_tokens_starter_100" },
-  { name: "Booster", price: 1.35, tokens: 300, stripePriceId: "price_tokens_booster_300" },
-  { name: "Writers", price: 2.70, tokens: 750, stripePriceId: "price_tokens_writers_750" },
-  { name: "Bard", price: 5.40, tokens: 2000, stripePriceId: "price_tokens_bard_2000" },
-]; 
