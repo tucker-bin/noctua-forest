ebd1702e888460a372b99cd192a344327addf2b9
tucker.apply@gmail.com <tucker.apply@gmail.com>
2025-09-01 01:51:04 -0400

    Export app/db from firebase-config as ESM; unify mobile menu link colors across pages

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
-// Firebase Web App config
-const firebaseConfig = {
+// Firebase Web App config (browser ESM)
+export const firebaseConfig = {
   apiKey: "AIzaSyDkrIV0Ji28ULmGHDl6mLTMvRWQsyR4XFg",
   authDomain: "my-rhyme-app.firebaseapp.com",
   projectId: "my-rhyme-app",
@@ -7,24 +7,13 @@ const firebaseConfig = {
   messagingSenderId: "487322724536",
   appId: "1:487322724536:web:a5eb91460b22e57e2b2c4d"
 };
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
 
-// Check if we're in a browser environment
-if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-  // Browser: Load Firebase via CDN
-  const script = document.createElement('script');
-  script.type = 'module';
-  script.textContent = `
-    import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
-    import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
-    const firebaseConfig = ${JSON.stringify(firebaseConfig, null, 2)};
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    window.__FIREBASE_APP__ = app;
-    window.__FIREBASE_DB__ = db;
-  `;
-  document.head.appendChild(script);
-} else {
-  // Node.js: Export config only
-  module.exports = { firebaseConfig };
+if (typeof window !== 'undefined') {
+  window.__FIREBASE_APP__ = app;
+  window.__FIREBASE_DB__ = db;
 }
