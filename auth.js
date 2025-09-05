a7a98cfe9d5abff54479d092660d6087f3c052c4
tucker.apply@gmail.com <tucker.apply@gmail.com>
2025-09-04 20:28:57 -0400

    publish: site update

--- auth.js
+++ auth.js
@@ -314,7 +314,7 @@ async function loadReadingLists(userId) {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
                 </svg>
               </button>
-              <button onclick="deleteList('${list.id}')" class="p-2 text-white/60 hover:text-red-400 hover:bg-red-500/10 rounded">
+              <button onclick="deleteListHandler('${list.id}')" class="p-2 text-white/60 hover:text-red-400 hover:bg-red-500/10 rounded">
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                 </svg>
@@ -494,7 +494,7 @@ async function editList(listId) {
   showEditListModal(listId);
 }
 
-async function deleteList(listId) {
+async function deleteListHandler(listId) {
   showDeleteConfirmModal(listId);
 }
 
